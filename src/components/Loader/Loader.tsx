3571d2b787e2b456845feee86793b9c46b0fb308
Oleksandr-Zibnytskyi <xander.en.87@gmail.com>
2024-07-04 11:04:51 +0200

    now

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,9 @@
 import { Audio } from "react-loader-spinner";
+import css from "./Loader.module.css"
 
 export default function Loader() {
   return (
-    <div>
+    <div className={css.loaderContainer}>
       <Audio height="80" width="80" color="blue" ariaLabel="loading" />
       <p>please wait...</p>
     </div>
